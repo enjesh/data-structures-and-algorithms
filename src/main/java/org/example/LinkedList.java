62235e1cc85d97117fb70d9455b3ce6da6bf1c20
Joseph Sarpong <joseph-sarpong@hotmail.com>
2023-05-12 08:45:12 +0000

    finish method to insert in list

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -2,18 +2,20 @@ package org.example;
 
 public class LinkedList <T>{
     private Node<T> head;
+    private int size = 0;
 
     public Node<T> getHead() {
         return this.head;
     }
 
-    public void addToHead(Node<T> newHead){
+    private void addToHead(Node<T> newHead){
         if (this.head != null)
             newHead.setNext(this.head);
         this.head = newHead;
+        size++;
     }
 
-    public void addToTail(Node<T> newTail){
+    private void addToTail(Node<T> newTail){
         if (this.head == null)
             this.head = newTail;
         else{
@@ -22,7 +24,36 @@ public class LinkedList <T>{
                 currentNode = currentNode.getNext();
             }
             currentNode.setNext(newTail);
+            size++;
         }
+    }
+
+    public void add(Node<T> node, int index){
+//      if index is greater than list size, insertion is impossible, throw error
+        if (size < index)
+            throw new IndexOutOfBoundsException("index is larger than list");
+        if (index == 0){
+            addToHead(node);
+        }
+//      if index is same as list size, add at tail
+        else if (size == index){
+            addToTail(node);
+        }
+        else{
+            Node<T> currentNode = this.head;
+            int counter = 1;
+            while (counter != index) {
+                currentNode = currentNode.getNext();
+                counter++;
+            }
+            node.setNext(currentNode.getNext());
+            currentNode.setNext(node);
+            size++;
+        }
+    }
+
+    public void add(Node<T> node){
+        addToTail(node);
     }
 
 }
--- src/test/java/org/example/LinkedListTest.java
+++ src/test/java/org/example/LinkedListTest.java
@@ -9,15 +9,15 @@ class LinkedListTest {
     @Test
     void Given_LinkedList_When_Empty_Then_AddToHeadShouldAddHeadToList() {
         LinkedList<String> list = new LinkedList<>();
-        list.addToHead(new Node<>("liz"));
+        list.add(new Node<>("liz"), 0);
         assertEquals("liz", list.getHead().getData());
     }
 
     @Test
     void Given_LinkedList_When_NotEmpty_Then_AddToHeadShouldChangeHeadOfList() {
         LinkedList<String> list = new LinkedList<>();
-        list.addToHead(new Node<>("liz"));
-        list.addToHead(new Node<>("Mart"));
+        list.add(new Node<>("liz"), 0);
+        list.add(new Node<>("Mart"), 0);
         assertEquals("Mart", list.getHead().getData());
         assertTrue(list.getHead().hasNext());
     }
@@ -29,9 +29,9 @@ class LinkedListTest {
         Node<String> major = new Node<>("Major");
         Node<String> mart = new Node<>("Mart");
 
-        list.addToHead(liz);
-        list.addToHead(mart);
-        list.addToTail(major);
+        list.add(liz, 0);
+        list.add(mart, 0);
+        list.add(major);
 
         assertEquals(mart, list.getHead());
         assertEquals(major, liz.getNext());
@@ -42,8 +42,67 @@ class LinkedListTest {
     void Given_LinkedList_When_Empty_Then_AddToTailShouldChangeTailOfList() {
         LinkedList<String> list = new LinkedList<>();
         Node<String> major = new Node<>("Major");
-        list.addToTail(major);
+        list.add(major);
 
         assertEquals(major, list.getHead());
+    }
+
+    @Test
+    void Given_LinkedList_When_SizeIs2_Then_AddAtIndex1ShouldAddNodeAtIndex1() {
+        LinkedList<String> list = new LinkedList<>();
+        Node<String> liz = new Node<>("liz");
+        Node<String> major = new Node<>("Major");
+        Node<String> mart = new Node<>("Mart");
+
+        list.add(liz, 0);
+        list.add(mart, 0);
+        list.add(major, 1);
+
+        assertEquals(major, mart.getNext());
+        assertEquals(liz, major.getNext());
+        assertNull(liz.getNext());
+    }
+
+    @Test
+    void Given_LinkedList_When_SizeIs2_Then_AddAtIndex5ShouldThrowAnException() {
+        LinkedList<String> list = new LinkedList<>();
+        Node<String> liz = new Node<>("liz");
+        Node<String> major = new Node<>("Major");
+        Node<String> mart = new Node<>("Mart");
+
+        list.add(liz, 0);
+        list.add(mart, 0);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(major, 5));
+    }
+
+    @Test
+    void Given_LinkedList_When_SizeIs2_Then_AddAtIndex2ShouldAddToTail() {
+        LinkedList<String> list = new LinkedList<>();
+        Node<String> liz = new Node<>("liz");
+        Node<String> major = new Node<>("Major");
+        Node<String> mart = new Node<>("Mart");
+
+        list.add(liz, 0);
+        list.add(mart, 0);
+        list.add(major, 2);
+
+        assertEquals(major, liz.getNext());
+        assertNull(major.getNext());
+    }
+
+    @Test
+    void Given_LinkedList_When_SizeIs2_Then_AddAtIndex0ShouldAddToHead() {
+        LinkedList<String> list = new LinkedList<>();
+        Node<String> liz = new Node<>("liz");
+        Node<String> major = new Node<>("Major");
+        Node<String> mart = new Node<>("Mart");
+
+        list.add(liz, 0);
+        list.add(mart, 0);
+        list.add(major, 0);
+
+        assertEquals(major, list.getHead());
+        assertEquals(mart, major.getNext());
     }
 }
